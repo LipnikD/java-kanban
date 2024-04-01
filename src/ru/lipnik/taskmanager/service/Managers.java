e8529aca49d235e89097dda33b6ddd395ae341e4
Lipnik Dmitry <lipnik-dimm@yandex.ru>
2024-04-01 22:23:37 +0300

    Changes after review (Test Fix)

--- src/ru/lipnik/taskmanager/service/Managers.java
+++ src/ru/lipnik/taskmanager/service/Managers.java
@@ -3,7 +3,7 @@ package ru.lipnik.taskmanager.service;
 public class Managers {
 
     public static TaskManager getDefault() {
-        return new FileBackedTaskManager();
+        return new InMemoryTaskManager();
     }
 
     public static HistoryManager getDefaultHistory() {
